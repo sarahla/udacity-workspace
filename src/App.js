505cc0ec9eb69cb9f6af849932f38cfbf7784251
Sarah Larson <contact@sarahelarson.com>
2019-11-15 01:35:28 +0000

    Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MovieList from './MovieList';
 import './App.css';
 import logo from './logo.svg';
 
@@ -106,6 +107,7 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<MovieList profiles={profiles} movies={movies} users={users} /> 
       </div>
     );
   }
